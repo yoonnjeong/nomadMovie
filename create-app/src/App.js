647af11a1ebae5a9aab902cd1d8b6c4aa5d710c5
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-01 17:55:47 +0900

    create react app recap

---
+++ create-app/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>back!!</h1>
+      <Button text={"bigbig"} />
+    </div>
+  );
+}
+
+export default App;
