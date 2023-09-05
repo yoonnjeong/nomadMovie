08c6124e3a40824c01be37d09967e91d6a8cd8b5
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-05 16:03:17 +0900

    todo 만들기

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -2,19 +2,42 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const onChange = (e) => {
-    setToDo(e.target.value);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+
+    // setToDos(function (currentArray) {
+    //   return;
+    // }); //아래와 같은 내용이다
+
+    setToDos((currentArray) => [toDo, ...toDos]);
+    console.log(toDos);
+
+    // setToDos((currentArray) => {
+    //   const newArray = [toDo, ...currentArray];
+    //   console.log(newArray);
+    //   return newArray;
+    // });   //return처리를 해주면 입력한 값이 바로 배열에 추가되서 출력된다
   };
 
   return (
     <div>
-      <input
-        value={toDo}
-        onChange={onChange}
-        type="text"
-        placeholder="Write your to do..."
-      />
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
