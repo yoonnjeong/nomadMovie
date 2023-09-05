8bcf51a78f53a297c79a1b37e09e2cefa5570f5b
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-05 12:20:45 +0900

    input form

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+
+  const onChange = (e) => {
+    setToDo(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        value={toDo}
+        onChange={onChange}
+        type="text"
+        placeholder="Write your to do..."
+      />
+    </div>
+  );
 }
 
 export default App;
