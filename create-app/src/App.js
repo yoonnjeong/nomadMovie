7565a1c7f0f575ce849818f5c622d28cf8d03a8f
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-05 12:05:44 +0900

    reset

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,43 +1,5 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  //type 1
-  function effectFn() {
-    console.log("created");
-    return destroyedFn;
-  }
-  function destroyedFn() {
-    console.log("destroyed");
-  }
-  useEffect(effectFn, []);
-
-  //type 2
-  useEffect(() => {
-    console.log("hi");
-    return () => console.log("bye");
-  }, []);
-
-  //type 3
-  useEffect(function () {
-    console.log("hi");
-    return function () {
-      console.log("bye");
-    };
-  }, []);
-
-  return <h1>Hi</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
