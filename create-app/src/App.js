bf4bd969e34e472db08770a7788a2c50dcf3dee3
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-06 16:50:19 +0900

    movie data async await

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -3,17 +3,30 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovie] = useState([]);
+  // const getMovies = async () => {
+  //   const res = await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   );
+  //   const json = await res.json();
+
+  //   setMovie(json.data.movies);
+  //   setLoading(false);
+  // }; // 아래외 같은 방법이다
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovie(json.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setMovie(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
+
   console.log(movies);
 
   return <div>{loading ? <h1>Loading</h1> : null}</div>;
