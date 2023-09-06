e7219e168bbbc2f22c7f0b2a602f520c8fcef204
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-06 14:42:28 +0900

    coin code challenge

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -3,6 +3,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState("");
+  const [coinValue, setCoinValue] = useState(1);
+
+  const onChange = (e) => {
+    setMoney(e.target.value);
+  };
+
+  const onCoinValue = (e) => {
+    setCoinValue(e.target.value);
+  };
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -15,15 +25,31 @@ function App() {
 
   return (
     <div>
-      <h1>The Coins{loading ? "" : `(${coins.length})`} ~ !</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-          </li>
-        ))}
-      </ul>
+      <h1>The all Coins {loading ? "" : `(${coins.length})`} ~ !</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <h2>💰 How much your Coin? Select the Coin!!</h2>
+          <input
+            type="number"
+            value={money}
+            onChange={onChange}
+            placeholder="Write your money!"
+          />
+          $
+          <hr />
+          <select onChange={onCoinValue}>
+            <option>Select Option</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <h3>Get Coin {money / coinValue}</h3>
+        </div>
+      )}
     </div>
   );
 }
