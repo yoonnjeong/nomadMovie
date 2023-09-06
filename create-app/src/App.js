ca1891e2b1d3f5727d58be000fdd3b6a933bdc7d
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-06 16:30:20 +0900

    movie data then catch type

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setMovie(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
+
+  return <div>{loading ? <h1>Loading</h1> : null}</div>;
 }
 
 export default App;
