19da39e40251e3510261b46ee301d1ff6657e4b7
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-06 10:13:25 +0900

    reset code

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,41 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-
-    setToDos((currentArray) => [toDo, ...toDos]);
-  };
-  console.log(toDos);
-
-  return (
-    <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
