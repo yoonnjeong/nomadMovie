8345bdad869c6572dccb525004bd590b8f60ddcc
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-06 11:33:23 +0900

    make coin list

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>The Coins{loading ? "" : `(${coins.length})`} ~ !</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
