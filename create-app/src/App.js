dd2bfc4fffa4d76432e0309d8123d1b44fe88606
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-07 14:14:57 +0900

    react router setting

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,54 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovie] = useState([]);
-  // const getMovies = async () => {
-  //   const res = await fetch(
-  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-  //   );
-  //   const json = await res.json();
-
-  //   setMovie(json.data.movies);
-  //   setLoading(false);
-  // }; // 아래외 같은 방법이다
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovie(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              mediumCoverImage={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ create-app/src/components/Movie.js
@@ -0,0 +1,33 @@
+import propTypes from "prop-types";
+
+function Movie({ mediumCoverImage, title, summary, genres }) {
+  const handleError = (e) => {
+    //이미지를 못찾을경우 다른 엘리먼트 생성 후에 이미지가 있던 맨 앞자리에 새로운 엘리먼트 생성하기
+    e.target.style.display = "none";
+    const errorText = document.createElement("h3");
+    errorText.textContent = "No Img";
+
+    const parent = e.target.parentNode;
+    parent.insertBefore(errorText, e.target);
+  };
+
+  return (
+    <div>
+      <img src={mediumCoverImage} onError={handleError} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres && genres.map((genre, idx) => <li key={idx}>{genre}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  mediumCoverImage: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
