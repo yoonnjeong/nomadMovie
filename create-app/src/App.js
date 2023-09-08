6585fb073cdd10c07f3ec2bc929f91aaf345aa36
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2023-09-08 09:21:27 +0900

    파일 에러 수정

--- create-app/src/App.js
+++ create-app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./routes/Home";
+import Home from "./routes/Home.js";
 import Detail from "./Detail";
 
 function App() {
